ced1c222ba39652eee849947d2e844f168364e9c
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-17 19:05:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(arr)
   n = 0
   arr = []
   while n < arr.length
-  	yield(arr[n]) #should we add whatever this returns to the array?
+  arr.push	(yield(arr[n])) #should we add whatever this returns to the array?
   	n += 1
   end
   arr
